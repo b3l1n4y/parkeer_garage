cf571d3a088416b9ef74270853b460d215ad8195
b3l1n4y <148344846+b3l1n4y@users.noreply.github.com>
2025-06-03 15:35:39 +0200

    Add files via upload

---
+++ PlatformIO/GATE_CONTROL/src/secret_IoT.h
@@ -0,0 +1,11 @@
+// arduino_secrets.h header file
+const char *ssid = "IoT";
+const char *password = "KdGIoT83!";
+
+#define ssid "IoT"
+#define password "KdGIoT83!"
+
+// Airtable Configuration
+const char *airtableApiKey = "patFHRDQicoVujdu7.db8d40a5cd6f82d08516d56cb9f9299492a0e68ae8d97131217d5e4b16b08bdd";
+const char *baseId = "app2mOWaxPlGj12P4";
+const char *tableId = "tbltBMsnZK9V5KFlb"; // Your Sensors table ID
